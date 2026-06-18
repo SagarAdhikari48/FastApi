9f3b006c5446a383de14d7c9b4a9442d61664dce
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-18 16:47:07 +0545

    About endpoint added

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "My first API is working!"}
+
+
+@app.get("/about")
+def about():
+    return {"project": "Loan risk model", "version": "1.0"}
