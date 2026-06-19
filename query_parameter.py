9f929cce07d34cb87476586f83b359b4e4c8a286
Sagar Adhikari <sagradhkr48@gmail.com>
2026-06-19 16:52:04 +0545

    When to use query vs path parameter

--- query_parameter.py
+++ query_parameter.py
@@ -1,4 +1,5 @@
 # Query Parameters - > value attached at the end of the path parameter
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -16,3 +17,11 @@ all_customers = [
 def get_customer(city: str, risk: str):
     filtered = [c for c in all_customers if (c["city"] == city and c["risk"] == risk)] 
     return {"city": city, "risk": risk, "count": len(filtered), "results": filtered}
+
+
+### Path parameter vs Query parameter
+#Path - > specific things to find like which model ,which customer, which report
+##query Parameters - > filtering, sorting, customise
+
+# /customer can send all customer which will crash the server so please send limit as
+# limit = 10 not limit: int . there should be default value
